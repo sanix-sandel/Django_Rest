e5f643610ed2abde77fdd9b744c7ded74e436d08
sanix <sanicksikani@gmail.com>
2021-01-15 06:29:55 +0300

    models and views

---
+++ games/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Game(models.Model):
+    created_timestamp = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=200, default='')
+    release_date = models.DateTimeField()
+    esrb_rating = models.CharField(max_length=150, default='')
+    played_once = models.BooleanField(default=False)
+    played_times = models.IntegerField(default=0)
+    class Meta:
+        ordering = ('name',)
