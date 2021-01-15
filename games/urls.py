d250efd5a19834b9a4deb4ca72d7e44890d28303
sanix <sanicksikani@gmail.com>
2021-01-15 08:21:47 +0300

    views and urls

--- games/urls.py
+++ games/urls.py
@@ -1,14 +1,14 @@
-from django.conf.urls import url
+from django.urls import path
 from games import views
 
 urlpatterns = [
-    url('esrb-ratings/',views.EsrbRatingList.as_view(), name=views.EsrbRatingList.name),
-    url('esrb-ratings/<int:pk>/', views.EsrbRatingDetail.as_view(), name=views.EsrbRatingDetail.name),
-    url('games/', views.GameList.as_view(), name=views.GameList.name),
-    url('games/<int:pk>/', views.GameDetail.as_view(), name=views.GameDetail.name),
-    url('players/', views.PlayerList.as_view(), name=views.PlayerList.name),
-    url('players/<int:pk>/', views.PlayerDetail.as_view(), name=views.PlayerDetail.name),
-    url('player-scores/', views.PlayerScoreList.as_view(), name=views.PlayerScoreList.name),
-    url('player-scores/<int:pk>/', views.PlayerScoreDetail.as_view(), name=views.PlayerScoreDetail.name),
-    url('', views.ApiRoot.as_view(), name=views.ApiRoot.name),
+    path('esrb-ratings/',views.EsrbRatingList.as_view(), name=views.EsrbRatingList.name),
+    path('esrb-ratings/<int:pk>/', views.EsrbRatingDetail.as_view(), name=views.EsrbRatingDetail.name),
+    path('games/', views.GameList.as_view(), name=views.GameList.name),
+    path('games/<int:pk>/', views.GameDetail.as_view(), name=views.GameDetail.name),
+    path('players/', views.PlayerList.as_view(), name=views.PlayerList.name),
+    path('players/<int:pk>/', views.PlayerDetail.as_view(), name=views.PlayerDetail.name),
+    path('player-scores/', views.PlayerScoreList.as_view(), name=views.PlayerScoreList.name),
+    path('player-scores/<int:pk>/', views.PlayerScoreDetail.as_view(), name=views.PlayerScoreDetail.name),
+    path('', views.ApiRoot.as_view(), name=views.ApiRoot.name),
 ]
